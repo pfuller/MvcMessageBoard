013aabd9d52dda062053bfc01790f2a7a4fe8901
pfuller <paul.fuller.uk@googlemail.com>
2017-11-23 18:35:33 +0000

    Added DB context and fixed styles

---
+++ MessageBoardWebApp/Data/MessageBoardContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MessageBoardWebApp.Data
+{
+    public class MessageBoardContext : DbContext
+    {
+        public MessageBoardContext() : base ("DefaultConnection")
+        {
+           
+        }
+
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Reply> Replies { get; set; }
+    }
+}
---
+++ MessageBoardWebApp/Data/Topic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MessageBoardWebApp.Data
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Body { get; set; }
+        public DateTime Created { get; set; }
+        public ICollection<Reply> Replies { get; set; }
+    }
+}
